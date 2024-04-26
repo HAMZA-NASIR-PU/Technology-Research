ead2482eceffb877b4327012b7028519eb824b4f
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-04-26 08:20:21 +0500

    Angular date and time picker testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DatePipe, JsonPipe } from '@angular/common';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { RouterOutlet } from '@angular/router';
     RouterOutlet,
     MatFormFieldModule,
     MatDatepickerModule,
+    MatInputModule,
     FormsModule,
     ReactiveFormsModule,
     JsonPipe,
