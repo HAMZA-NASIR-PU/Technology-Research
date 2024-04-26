b4a0788616201fe233b422648d80171f22fb4592
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-04-26 08:06:29 +0500

    Add Angular Material UI Datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe, JsonPipe } from '@angular/common';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    JsonPipe,
+    DatePipe
+  ],
+  providers: [provideNativeDateAdapter(), DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  constructor(private datePipe: DatePipe) {
+    this.range.valueChanges.subscribe(value => {
+      console.log("Date range changed = ", value);
+      console.log("Start Date = " + this.datePipe.transform(value.start, 'yyyy-MM-ddTHH:mm:ssZ'));
+      console.log("End Date = " + this.datePipe.transform(value.end, 'yyyy-MM-ddTHH:mm:ssZ'));
+    });
+  }
+
 }
